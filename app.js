790fd4ca254eade9c9501ed8492fafdbb8b4587d
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-07-14 17:47:59 -0500

    Add res.render method, Add programatic for loop for items in todo list

--- app.js
+++ app.js
@@ -5,57 +5,36 @@ const port = 3000;
 
 //body parser
 app.use(express.urlencoded({extended: true}));
+
+  
 app.set('view engine', 'ejs');
-// app.use(express.static( __dirname, + 'public'));
+
 
 app.listen(port, () => {
     console.log('Server listening on port: ' + port)
 });
 
+
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+
 app.get('/', (req, res) => {
-    // res.sendFile(__dirname + '/views/index.html');
+
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
 
    let currentDay = new Date();
-   currentDay = currentDay.getDay();
-
-   let day = '';
-
-   if(currentDay == 0 || currentDay == 6){
-    console.log(currentDay)
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday';
-            break;
-        case 6:
-            day = 'Saturday';
-            break;
-        default:
-            break;
-    }
-   }else{
-    switch (currentDay) {
-        case 1:
-            day = 'Monday';
-            break;
-        case 2:
-            day = 'Tuesday';
-            break;
-        case 3:
-            day = 'Wednesday';
-            break;
-        case 4:
-            day = 'Thrusday';
-            break;
-        case 5:
-            day = 'Friday';
-            break;
-        default:
-            throw new Error(console.log( currentDay + ' Not a vaild day'))
-            break;
-    }
-   }
-    
-    res.render('lists', {day:day});
+   let day = currentDay.toLocaleDateString('en', options)
+    res.render('lists', {day:day, items:items});
 
 
+})
+
+app.post('/', (req, res) =>{
+    let newItem = req.body.item;
+    items.push(newItem);
+
+    res.redirect("/");
 })
